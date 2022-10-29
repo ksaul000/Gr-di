22901015df9a87396fefbe19e4f5a1ecfb991456
Hopartean Mihaela-Lidia <mihaelahopartean@gmail.com>
2022-10-29 22:03:12 +0300

    Initial Homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AriciComponent } from './arici/arici.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AriciComponent
+    AriciComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule
