7b4d9666b272de91626067ca3ad9b56517d4c390
Kocsis Saul <ksaulfelix@yahoo.com>
2022-10-31 16:30:55 +0200

    select dutton added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AriciComponent } from './arici/arici.component';
+import { TryComponent } from './try/try.component';
+import { SelectButtonComponent } from './select-button/select-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AriciComponent
+    AriciComponent,
+    TryComponent,
+    SelectButtonComponent
   ],
   imports: [
     BrowserModule
