f3f09a4f2dd486d3f5aebd314f15795de80908b0
Ali Akan <ali.akan.narcis01@gmail.com>
2022-11-02 22:24:14 +0200

    background image + main app frame

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { AriciComponent } from './arici/arici.component';
+import { BingoComponent } from './bingo/bingo.component';
+// import { AriciComponent } from './arici/arici.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AriciComponent
+    BingoComponent
   ],
   imports: [
     BrowserModule
