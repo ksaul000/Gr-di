ff9ce78e53db5b31d2b71efc50e914ba88c2f8c3
Kocsis Saul <ksaulfelix@yahoo.com>
2022-11-13 18:26:43 +0200

    show right selected answers when press the correct button

--- src/app/level1/level1.component.ts
+++ src/app/level1/level1.component.ts
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./level1.component.css']
 })
 export class Level1Component implements OnInit {
-
+  answer1_visible: boolean = false;
+  answer2_visible: boolean = false;
+  answer3_visible: boolean = false;
+  correct_answer_button1_visible: boolean = true;
+  correct_answer_button2_visible: boolean = true;
+  correct_answer_button3_visible: boolean = true;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  clickOnCorrectButton1(): void{
+    if(this.correct_answer_button1_visible){
+      this.correct_answer_button1_visible=false;
+      this.answer1_visible=true;
+    }
+  }
+
+  clickOnCorrectButton2(): void{
+    if(this.correct_answer_button2_visible){
+      this.correct_answer_button2_visible=false;
+      this.answer2_visible=true;
+    }
+  }
+
+  clickOnCorrectButton3(): void{
+    if(this.correct_answer_button3_visible){
+      this.correct_answer_button3_visible=false;
+      this.answer3_visible=true;
+    }
   }
 
 }
