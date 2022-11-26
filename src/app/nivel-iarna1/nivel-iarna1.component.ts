255c6cf7bc8f69ba425adc52554283b6ae9dadb0
moldovan-andreea2 <115736043+moldovan-andreea2@users.noreply.github.com>
2022-11-26 11:24:15 +0200

    Added emotes for winter level

--- src/app/nivel-iarna1/nivel-iarna1.component.ts
+++ src/app/nivel-iarna1/nivel-iarna1.component.ts
@@ -13,7 +13,7 @@ export class NivelIarna1Component implements OnInit {
   correct_answer_button1_visible: boolean = true;
   correct_answer_button2_visible: boolean = true;
   correct_answer_button3_visible: boolean = true;
-  emote_image: string = "../../assets/curious_girl.jpg";
+  emote_image: string = "../../assets/nivel_iarna1/inceput.jpg";
   audio = new Audio;
   happy_wav_path: string = "../../assets/yay_sound_effect.wav";
   no_sound_effect: string = "../../assets/no_sound_effect.wav";
@@ -44,7 +44,7 @@ export class NivelIarna1Component implements OnInit {
     if(this.correct_answer_button1_visible){
       this.correct_answer_button1_visible=false;
       this.answer1_visible=true;
-      this.emote_image = "../../assets/happy_girl.jpg";
+      this.emote_image = "../../assets/nivel_iarna1/corect.jpg";
       this.audio.src = this.happy_wav_path;
       this.audio.load();
       this.audio.play();
@@ -59,7 +59,7 @@ export class NivelIarna1Component implements OnInit {
     if(this.correct_answer_button2_visible){
       this.correct_answer_button2_visible=false;
       this.answer2_visible=true;
-      this.emote_image = "../../assets/happy_girl.jpg"
+      this.emote_image = "../../assets/nivel_iarna1/corect.jpg"
       this.audio.src = this.happy_wav_path;
       this.audio.load();
       this.audio.play();
@@ -74,7 +74,7 @@ export class NivelIarna1Component implements OnInit {
     if(this.correct_answer_button3_visible){
       this.correct_answer_button3_visible=false;
       this.answer3_visible=true;
-      this.emote_image = "../../assets/happy_girl.jpg"
+      this.emote_image = "../../assets/nivel_iarna1/corect.jpg"
       this.audio.src = this.happy_wav_path;
       this.audio.load();
       this.audio.play();
@@ -86,7 +86,7 @@ export class NivelIarna1Component implements OnInit {
   }
 
   clickOnWrongButton(): void{
-    this.emote_image = "../../assets/upset_girl.jpg"
+    this.emote_image = "../../assets/nivel_iarna1/gresit.jpg"
     this.audio.src = this.  no_sound_effect;
     this.audio.load();
     this.audio.play();
